2acad284b41d0bbc11a9d8640dccb7e319bd41a7
hoang <vinhyenvodoi98@gmail.com>
2020-05-23 16:34:28 +0700

    feat : setup docker-compose rabbitmq

--- MQTT_Broker_nodejs/publisher.js
+++
@@ -1,9 +0,0 @@
-var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://192.168.43.46');
-
-client.on('connect', () => {
-  setInterval(() => {
-    client.publish('myTopic', 'Hello mqtt');
-    console.log('Message Sent');
-  }, 5000);
-});
