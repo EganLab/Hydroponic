e39506431c88404e6a2c0d73aaa126407735fe4c
hoang <vinhyenvodoi98@gmail.com>
2020-06-20 16:56:34 +0700

    feat : get data by minute

--- backend/routes/sensors.js
+++ backend/routes/sensors.js
@@ -1,19 +1,42 @@
 const router = require('express').Router();
 // const auth = require('../middleware/auth');
 const Sensor = require('../models/Sensor');
-// const Tracking = require('../models/Tracking');
+const Tracking = require('../models/Tracking');
 
 // get tracking by minute
-// router.get('/minute/:id', async (req, res) => {
-//   let deviceId = req.params.id;
-//   // TODO need more authorization
-//   try {
-//     const sensorData = await Sensor.find({ deviceId: deviceId });
-//     console.log(sensorData);
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// });
+router.get('/minute/:id', async (req, res) => {
+  let deviceId = req.params.id;
+  // TODO need more authorization
+  try {
+    const sensorDatas = await Sensor.find({ deviceId: deviceId });
+
+    var allSensorData = [];
+
+    await Promise.all(
+      sensorDatas.map(async sensorData => {
+        var temp = {};
+        let data = [];
+        let time = [];
+        temp.name = sensorData.name;
+        await Promise.all(
+          sensorData.dataSets.map(async data_hour => {
+            let data_minute = await Tracking.findById({ _id: data_hour._id });
+            data = data.concat(data_minute.data);
+            time = time.concat(data_minute.created_at);
+            Promise.resolve(data);
+          })
+        );
+        temp.data = data;
+        temp.time = time;
+        allSensorData.push(temp);
+        Promise.resolve(allSensorData);
+      })
+    );
+    res.status(201).json(allSensorData);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 // get tracking by hour
 router.get('/hour/:id', async (req, res) => {
